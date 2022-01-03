88d5e3692d6c6114697af3f1e817645223469572
AYOUB DRIOUICH <adcover13@M1-MacBook-Air.local>
2022-01-03 16:29:43 +0100

    fresh normed fish

--- includes/includes.h
+++ includes/includes.h
@@ -29,7 +29,7 @@ typedef struct s_info
 	int	nb_ra;
 	int	nb_rra;
 	int	nb_rrb;
-	int     num;
+	int	num;
 	int	nb_rr;
 	int	nb_rrr;
 	int	t;
@@ -54,7 +54,8 @@ int	ft_strcmp(char *s1, char *s2);
 int	check_args(char ***args, int *ac);
 
 //************pushswaputils********
-t_list	*form_stack(int *array, int *sorted, int ac);
+
+t_list		*form_stack(int *array, int *sorted, int ac);
 int	*sort_(char **args, int ac, int **res);
 int	small_stack(t_list **stack_a, int count);
 int	check_double_count(char **av, int c);
@@ -72,12 +73,11 @@ int	set_rr(t_list **stack_a, t_list **stack_b, t_info strct);
 int	choose_op(t_list **stack_a, t_list **stack_b, t_info strct);
 
 //***********counters.c************
-int	nb_rb(t_list *stack_b, t_stack stacks_ab, int num);
-int	nb_ra(t_list *stack_a, int num);
-int	total(t_info strct);
-void 	max_to_top(t_list **stack_b, int nb_t, char *s1, char *s2);
-
-void 	do_(t_list **stack_b, int nb_t, int (*f)(), char *s);
-int	large_stack(t_list **stack_a, t_list **stack_b);
-int	max_uptop(t_list *stack_b);
+void	max_to_top(t_list **stack_b, int nb_t, char *s1, char *s2);
+void	do_(t_list **stack_b, int nb_t, int (*f)(), char *s);
+int		nb_rb(t_list *stack_b, t_stack stacks_ab, int num);
+int		nb_ra(t_list *stack_a, int num);
+int		total(t_info strct);
+int		large_stack(t_list **stack_a, t_list **stack_b);
+int		max_uptop(t_list *stack_b);
 #endif
